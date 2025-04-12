1114bf4f28124900f698f57405632b90bb59eab6
emeliesv <emeliesv@live.se>
2025-04-12 12:10:26 +0200

    knapp för rsvp i footern

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
-/* import Button from "./Button"; */
+import Button from "./Button";
 import Image from "./ImageComponent";
-/* import RSVP from "./RSVP";
+import RSVP from "./RSVP";
 
-import { useModal } from "../context/DialogContext"; */
+import { useModal } from "../context/DialogContext";
 
 import cake from "/assets/tårta.png?url";
 import bottlewGlass from "/assets/flaskaoglas.png?url";
@@ -13,7 +13,7 @@ import lemons from "/assets/citroner.png?url";
 import tower from "/assets/bubbeltorn.png?url";
 
 const Footer = () => {
-  /*   const { openModal } = useModal(); */
+  const { openModal } = useModal();
 
   return (
     <footer className="flex flex-col justify-center items-center py-10 px-6 md:px-0 bg-jossanKalleAccent">
@@ -23,35 +23,42 @@ const Footer = () => {
         vi kan behöva veta. <br />
         <b>OBS! boende fixar man själv.</b>
       </p>
-      {/* 
-      <Button onClickFunction={openModal}>RSVP</Button>
-      <RSVP /> */}
-      <p className="py-4 w-full md:text-center">NU BÖRJAR VI NEDRÄKNINGEN! </p>
-      <p className="pb-4 w-full md:text-center">Réspondez s'il vous plaît</p>
-      <div className="flex flex-row flex-wrap md:flex-nowrap items-baseline justify-center">
-        <Image
-          srcUrl={cake}
-          altText=""
-          hidden={true}
-          customClass="h-28 w-auto md:h-56"
-        />
-        <Image
-          srcUrl={bottlewGlass}
-          altText=""
-          hidden={true}
-          size="small"
-          customClass="w-28"
-        />
-        <Image
-          srcUrl={candle}
-          altText=""
-          hidden={true}
-          customClass="h-24 w-auto md:h-56"
-        />
-        <Image srcUrl={tomatos} altText="" hidden={true} size="xsmall" />
-        <Image srcUrl={plate} altText="" hidden={true} size="small" />
-        <Image srcUrl={lemons} altText="" hidden={true} size="xsmall" />
-        <Image srcUrl={tower} altText="" hidden={true} size="small" />
+      <div className="flex flex-col gap-5 justify-center items-center">
+        <Button onClickFunction={openModal}>RSVP</Button>
+        <RSVP />
+        <div className="flex flex-col gap-3">
+          <p className="py-4 w-full md:text-center">
+            NU BÖRJAR VI NEDRÄKNINGEN! 
+          </p>
+          <p className="pb-4 w-full md:text-center">
+            Réspondez s'il vous plaît
+          </p>
+        </div>
+        <div className="flex flex-row flex-wrap md:flex-nowrap items-baseline justify-center">
+          <Image
+            srcUrl={cake}
+            altText=""
+            hidden={true}
+            customClass="h-28 w-auto md:h-56"
+          />
+          <Image
+            srcUrl={bottlewGlass}
+            altText=""
+            hidden={true}
+            size="small"
+            customClass="w-28"
+          />
+          <Image
+            srcUrl={candle}
+            altText=""
+            hidden={true}
+            customClass="h-24 w-auto md:h-56"
+          />
+          <Image srcUrl={tomatos} altText="" hidden={true} size="xsmall" />
+          <Image srcUrl={plate} altText="" hidden={true} size="small" />
+          <Image srcUrl={lemons} altText="" hidden={true} size="xsmall" />
+          <Image srcUrl={tower} altText="" hidden={true} size="small" />
+        </div>
       </div>
     </footer>
   );
